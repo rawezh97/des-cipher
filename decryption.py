dc95f5e5381f38b44c357df0f1342ddec9ea5bd3
Rawe Zh <92225352+rawezh97@users.noreply.github.com>
2023-01-13 06:15:17 -0500

    updated

--- decryption.py
+++ decryption.py
@@ -66,21 +66,26 @@ def decryption(value,key):
 
 ########################################################################################
 #Add this part for fix the bin bug
+    print("abas")
+    print(len(decimalToBinary(int(regh_data))))
 
-if (len(decimalToBinary(int(lef_data))) <2):
-    left = "0" + decimalToBinary(int(lef_data)
-
-if (len(decimalToBinary(int(regh_data))) <2):
-    right = "0" + decimalToBinary(int(regh_data))
+    if len(decimalToBinary(int(lef_data)))<2:
+        left = "0" + decimalToBinary(int(lef_data))
+    else :
+        left = decimalToBinary(int(lef_data))
+    if len(decimalToBinary(int(regh_data)))<2:
+        right = "0" + decimalToBinary(int(regh_data))
+    else : 
+        right =decimalToBinary(int(regh_data))
                       
-Emurate_f = left + right
-    
-    
+    Emurate_f = left + right
+        
+
 ########################################################################################
     
     
-    #Emurate_f = decimalToBinary(int(lef_data)) + decimalToBinary(int(regh_data)) # 1111
-    # print("Emurate_f : " , Emurate_f)
+    # Emurate_f = decimalToBinary(int(lef_data)) + decimalToBinary(int(regh_data)) # 1111
+    #print("Emurate_f : " , Emurate_f)
 
 
     # R1 = L0 XOR f(R0,k1),R0
